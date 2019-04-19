52faeb0e6bc680c6c4222f8235d98fded0f67a34
Nesib Fiso <nesibf@hotmail.com>
2019-04-19 09:54:22 +0200

    added a new title

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    // apiKey: "AIzaSyCrJx--TXd_dnahkkp6xnUv8FOgTbBADb0",
-    // authDomain: "react-7f2af.firebaseapp.com",
-    // databaseURL: "https://react-7f2af.firebaseio.com",
+    apiKey: "AIzaSyCrJx--TXd_dnahkkp6xnUv8FOgTbBADb0",
+    authDomain: "react-7f2af.firebaseapp.com",
+    databaseURL: "https://react-7f2af.firebaseio.com",
 });
 
 const base = Rebase.createClass(firebaseApp.database());
